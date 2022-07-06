314b5a7d366cce85f8d9b9eefa5101b3ada5fb11
hyunsangwon <hyunsangwon93@gmail.com>
2022-07-06 09:53:27 +0900

    feat : 청약 맵 마커 기능 추가

--- src/components/MarkerMap/Marker.js
+++ src/components/MarkerMap/Marker.js
@@ -1,35 +1,47 @@
-import React from 'react';
+import { React, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
-import { positions } from '../../common/data';
 import Nav from '../Nav/Nav';
+import { getApartmentsSubscription } from '../../features/apartments/apartmentSlice';
+import { fetchAsyncApartmentsSubscription } from '../../features/apartments/apartmentSlice';
+import { center } from '../../common/map-setting';
 
+// 청약 데이터 마커 맵
 const Marker = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchAsyncApartmentsSubscription());
+  }, [dispatch]);
+
+  const data = useSelector(getApartmentsSubscription);
+
   return (
     <div>
       <Nav />
       <Map // 지도를 표시할 Container
-        center={{
-          // 지도의 중심좌표
-          lat: 33.450701,
-          lng: 126.570667,
-        }}
+        center={center}
         style={{ width: '100%', height: '100vh' }}
-        level={3} // 지도의 확대 레벨
+        level={7} // 지도의 확대 레벨
       >
-        {positions.map((position, index) => (
-          <MapMarker
-            key={`${position.title}-${position.latlng}`}
-            position={position.latlng} // 마커를 표시할 위치
-            image={{
-              src: 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png', // 마커이미지의 주소입니다
-              size: {
-                width: 24,
-                height: 35,
-              }, // 마커이미지의 크기입니다
-            }}
-            title={position.title} // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-          />
-        ))}
+        {data.length === undefined
+          ? console.log('map init...')
+          : data.map((res) => (
+              <MapMarker
+                key={res.subscriptionNo}
+                position={{
+                  lat: res.latitude,
+                  lng: res.longitude,
+                }}
+                image={{
+                  src: 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png', // 마커이미지의 주소입니다
+                  size: {
+                    width: 30,
+                    height: 40,
+                  },
+                }}
+                title={res.apartmentsName + ' (' + res.apartmentsSubscriptionDate + ')'}
+              />
+            ))}
       </Map>
     </div>
   );
