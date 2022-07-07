900b52576713427f2111f143a26604045d9f1ea0
hyunsangwon <hyunsangwon93@gmail.com>
2022-07-07 10:08:44 +0900

    fix : 경로 수정

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home/Home';
+import Home from './components/home/Home';
 import Marker from './components/MarkerMap/Marker';
 import Clusterer from './components/ClustererMap/Clusterer';
 function App() {
