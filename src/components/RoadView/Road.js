7db718348f9e30c0f1619881329dabf0cc279b15
hyunsangwon <hyunsangwon93@gmail.com>
2022-07-08 10:04:01 +0900

    feat : 로드뷰 종료 버튼 생성

--- src/components/RoadView/Road.js
+++ src/components/RoadView/Road.js
@@ -1,11 +1,21 @@
 import { React } from 'react';
 import { useParams } from 'react-router';
 import { Roadview } from 'react-kakao-maps-sdk';
+import './Road.css';
+
+const moveHome = () => {
+  document.location.href = '/map';
+};
 
 const Road = () => {
   const { lat, lng } = useParams();
   return (
     <div>
+      <div className="info_roadview">
+        <div className="btn_close" onClick={moveHome}>
+          로드뷰 종료
+        </div>
+      </div>
       <Roadview // 로드뷰를 표시할 Container
         position={{
           // 지도의 중심좌표
